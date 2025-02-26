ce450de4cfd978fecf29bb9f37ceb77b305128a6
kchoudhari127 <kchoudhari127@gmail.com>
2025-02-26 17:31:27 +0530

    - Configure Swagger URL in config file and automate opening in default browser
    - Added Swagger URL to appsettings.json
    - Updated Program.cs to read the Swagger URL from appsettings.json
    - Automated opening of Swagger UI in the default browser on application start

--- src/Employee-Management.API/Program.cs
+++ src/Employee-Management.API/Program.cs
@@ -1,9 +1,17 @@
+using Employee_Management.Business.Interfaces;
+using Employee_Management.Business.Services;
+using Employee_Management.Core.Interfaces;
 using Employee_Management.Repository.Data;
+using Employee_Management.Repository.Repositories;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -11,12 +19,29 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+//Add Swagger
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+
+    // Read the URL from the configuration file
+    var swaggerUrl = app.Configuration.GetValue<string>("SwaggerSettings:Url");
+    var processStartInfo = new ProcessStartInfo
+    {
+        FileName = swaggerUrl,
+        UseShellExecute = true
+    };
+    Process.Start(processStartInfo);
+
 }
 
 app.UseHttpsRedirection();
